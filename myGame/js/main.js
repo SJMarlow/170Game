e54b3879586c4a93883a8d3ae2d01a7280774bb2
Guanchen Liu <gliu13@ucsc.edu>
2019-10-13 15:03:31 -0700

    ButtonAndBits version1
    I set up a simple version of how button and bits will work in phaser. I'll try to add a bit generator later today.

--- myGame/js/main.js
+++ myGame/js/main.js
@@ -20,6 +20,11 @@ window.onload = function() {
 var TitleScreen = function(game) {};
 TitleScreen.prototype = {
 	preload: function() {
+		game.load.image('rightArrow', 'assets/img/rightArrow.png');
+		game.load.image('leftArrow', 'assets/img/leftArrow.png');
+		game.load.image('downArrow', 'assets/img/downArrow.png');
+		game.load.image('sheet', 'assets/img/sheet.png');
+
 		
 	},
 	create: function() {
@@ -51,6 +56,7 @@ StoryScreen1.prototype = {
 			game.state.start('TutorialGameplay');
 	}
 }
+
 var TutorialGameplay = function(game) {};
 TutorialGameplay.prototype = {
 	preload: function() {
@@ -61,10 +67,41 @@ TutorialGameplay.prototype = {
 		text.setText("Tutorial Gameplay");
 		text.anchor.x = 0.5;
 		text.anchor.y = 0.5;
+
+		//Add by Guanchen Liu
+		//Version1
+		//Adding sheets and button to the scene
+		this.sheet = game.add.sprite(0,25,'sheet');
+		this.sheet.scale.x = 1.2;
+		this.sheet.scale.y = 0.9;
+
+		//Related to Prefab/Button.js
+		this.leftButton = new Button(game , 80, 70, 'leftArrow',1,'left');
+		game.add.existing(this.leftButton);
+
+		this.rightButton = new Button(game , 80, 160, 'rightArrow',1,'right');
+		game.add.existing(this.rightButton);
+
+		this.downButton = new Button(game , 80, 250, 'downArrow',1,'down');
+		game.add.existing(this.downButton);
+
+		//Add a test bit for the game
+		this.testBit1 = new Bit(game, 900, 70, 'leftArrow', 1, 'left', this.leftButton);
+		game.add.existing(this.testBit1);
+
+		this.testBit2 = new Bit(game, 1100, 160, 'rightArrow', 1, 'right', this.rightButton);
+		game.add.existing(this.testBit1);
+
+		this.testBit3 = new Bit(game, 1500, 250, 'downArrow', 1, 'down', this.downButton);
+		game.add.existing(this.testBit1);
+
+
 		game.stage.backgroundColor = "#412312";
 	},
 	update: function() {
-		if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+		//Change by Guanchen Liu
+		//I change the key to process the scene to SPACE key.
+		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACE))
 			game.state.start('StoryScreen2');
 	}
 }
