978ff3d1d8dae808edf97a4d2ddf5a17fe8d262c
DavidJMonroe <djmonroe@ucsc.edu>
2019-10-18 14:12:54 -0700

    Moved asset loading into load state
    Moved tutorial gameplay asset loading into the load state with the rest of the assets.

--- myGame/js/Scene/Load.js
+++ myGame/js/Scene/Load.js
@@ -1,8 +1,15 @@
 var Load = function(game) {};
 Load.prototype = {
 	preload: function() {
-		//Load Graphics Assets
+
+		//Load IMAGE Assets
 		game.load.path = 'assets/img/';
+
+		//tutorial assets
+		game.load.image('rightArrow', 'rightArrow.png');
+		game.load.image('leftArrow', 'leftArrow.png');
+		game.load.image('downArrow', 'downArrow.png');
+		game.load.image('sheet', 'sheet.png');
 
 		//story scene portaits
 		//Naming format: GameStateTitle ImageSide SequenceNumber
@@ -17,6 +24,12 @@ Load.prototype = {
 		game.load.image('StoryScene1TextRight1', 'Dialogue2.png');
 		game.load.image('StoryScene2TextLeft1', 'Dialogue2.png');
 		game.load.image('StoryScene2TextRight1', 'Dialogue1.png');
+
+		//Load AUDIO assets
+		game.load.path = 'assets/audio/';
+
+		//tutorial music
+		game.load.audio('bgMusic', '90bpmTutorial.mp3');
 	},
 	create: function() {
 		game.state.start('TitleScreen');
