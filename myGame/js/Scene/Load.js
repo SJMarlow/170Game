403637742b5599137a4f40f5cc8203285c6d2a8b
DavidJMonroe <djmonroe@ucsc.edu>
2019-10-18 13:27:54 -0700

    Naming revision and scene 2 implementation
    Changed variable names to be more modular for easier asset implementation later. Also setup the basic framework for the second scene (currently just reusing the same temp assets.)

--- myGame/js/Scene/Load.js
+++ myGame/js/Scene/Load.js
@@ -4,13 +4,19 @@ Load.prototype = {
 		//Load Graphics Assets
 		game.load.path = 'assets/img/';
 
-		//story demo portaits
-		game.load.image('Alphinaud', 'Alphinaud.png');
-		game.load.image('Exarch', 'Exarch.png');
+		//story scene portaits
+		//Naming format: GameStateTitle ImageSide SequenceNumber
+		game.load.image('StoryScene1ImageLeft1', 'Alphinaud.png');
+		game.load.image('StoryScene1ImageRight1', 'Exarch.png');
+		game.load.image('StoryScene2ImageLeft1', 'Exarch.png');
+		game.load.image('StoryScene2ImageRight1', 'Alphinaud.png');
 		
-		//story demo textboxes
-		game.load.image('Dialogue1', 'Dialogue1.png');
-		game.load.image('Dialogue2', 'Dialogue2.png');
+		//story scene textboxes
+		//Naming format: GameStateTitle TextSide SequenceNumber 
+		game.load.image('StoryScene1TextLeft1', 'Dialogue1.png');
+		game.load.image('StoryScene1TextRight1', 'Dialogue2.png');
+		game.load.image('StoryScene2TextLeft1', 'Dialogue2.png');
+		game.load.image('StoryScene2TextRight1', 'Dialogue1.png');
 	},
 	create: function() {
 		game.state.start('TitleScreen');
